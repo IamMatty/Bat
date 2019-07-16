0c79e9391421c0d2902d299eec50972aa5341e69
Djskycassio <52176599+Djskycassio@users.noreply.github.com>
2019-07-15 22:40:32 -0300

    new one att 007

---
+++ load bar.bat
@@ -0,0 +1,145 @@
+@echo off
+color f9
+mode 30,3
+
+
+:inicio
+
+echo.
+echo  ВБАБВллллллллллллллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лВБАБВлллллллллллллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллВБАБВллллллллллллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лллВБАБВлллллллллллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллллВБАБВллллллллллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лллллВБАБВлллллллллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллллллВБАБВллллллллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лллллллВБАБВлллллллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллллллллВБАБВллллллллллллллл
+ping localhost -n 1 > nul
+
+
+cls
+echo.
+echo  лллллллллВБАБВлллллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллллллллллВБАБВллллллллллллл
+ping localhost -n 1 > nul
+
+
+cls
+echo.
+echo  лллллллллллВБАБВлллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллллллллллллВБАБВллллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лллллллллллллВБАБВлллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллллллллллллллВБАБВллллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лллллллллллллллВБАБВлллллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лллллллллллллллллВБАБВлллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллллллллллллллллллВБАБВллллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лллллллллллллллллллВБАБВлллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллллллллллллллллллллВБАБВллл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лллллллллллллллллллллВБАБВлл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  ллллллллллллллллллллллВБАБВл
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  лллллллллллллллллллллллВБАБВ
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  БВлллллллллллллллллллллллВБА
+ping localhost -n 1 > nul
+
+
+cls
+echo.
+echo  АБВлллллллллллллллллллллллВБ
+ping localhost -n 1 > nul
+
+cls
+echo.
+echo  БАБВлллллллллллллллллллллллВ
+ping localhost -n 1 > nul
+
+
+goto inicio
+
+pause >nul
+
+ 
